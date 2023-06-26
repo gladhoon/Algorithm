315cbdc4cf541f8ca09a89254aac65f3b17e5479
jonas <sum9004@gmail.com>
2023-06-26 20:00:23 +0900

    2023.06.26

--- 14846.py
+++ 14846.py
@@ -1,22 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-arr = []
-position = []
-
 n = int(input())
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
+arr = [[0]*(n+1)] + [[0] + list(map(int, input().split())) for _ in range(n)]
 
-m = int(input())
-for _ in range(m):
-    position.append(list(map(int, input().split())))
+dp = [[[0] * 10 for _ in range(n+1)] for _ in range(n+1)]
 
-for i in range(m):
-    answer = []
-    x1, y1, x2, y2 = position[i][0]-1, position[i][1]-1, position[i][2]-1, position[i][3]-1
-    for y in range(y2-y1+1):
-        for x in range(x2-x1+1):
-            if arr[x][y] not in answer:
-                answer.append(arr[x][y])
-    print(len(answer))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        for k in range(1,10):
+            if k == arr[i][j]:
+                dp[i][j][k] += 1
+            dp[i][j][k] += dp[i-1][j][k] + dp[i][j-1][k] - dp[i-1][j-1][k]
+
+q = int(input())
+for _ in range(q):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = 0
+    for k in range(1,10):
+        if dp[x2][y2][k] - dp[x2][y1-1][k] - dp[x1-1][y2][k] + dp[x1-1][y1-1][k]:
+            result += 1
+    print(result)
